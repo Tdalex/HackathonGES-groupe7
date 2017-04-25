d36035a792b99f36ba9037e627a660d5ca69641c
Dalex Tanguy <dalex.tanguy@gmail.com>
2017-04-25 10:44:13 +0200

    add doctrine composer

---
+++ controllers/Controller.class.php
@@ -0,0 +1,40 @@
+<?php
+
+abstract class controller{
+
+	public function __construct(){
+	}
+
+	public function redirect($location = serverUrl){
+		if($location == 'home'){
+			$location = serverUrl;
+		}elseif(substr($location, 0, 4) != 'http'){
+			$location = serverUrl . $location;			
+		}
+
+		return header('Location:'. $location);
+	}
+
+	public static function dbConnect(){
+		try {
+			if(isset($_SERVER['HTTPS'])){
+				$dbh = new PDO('mysql:host=localhost;dbname=gfi_play', 'root', '');
+			}else{
+				$dbh = new PDO('mysql:host=localhost;dbname=gfi_play', 'root');
+			}
+			
+			return $dbh;
+		} catch (PDOException $e) {
+			print "Erreur !: " . $e->getMessage() . "<br/>";
+			die();
+		}
+	}
+	
+    public static function getInfoUserByLocalId($idUser){
+        $dbh = controller::dbConnect();
+        $sth = $dbh->prepare('SELECT * FROM user WHERE id_user = '. $idUser);
+        $sth ->execute();
+        return $sth->fetch();
+    }
+
+}
