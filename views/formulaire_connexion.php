f018ee44a4367086561113f91150e4ab9c7ad0af
tmeksava <teddy.meksavanh@arkena.com>
2017-04-26 18:16:03 +0200

    Connexion linked

--- views/formulaire_connexion.php
+++ views/formulaire_connexion.php
@@ -1,5 +1,26 @@
-<form action="" method="post">
-    <label for="mail">Email</label><input name="mail" id="mail" type="text">
-    <label for="mdp">Mot de passe</label><input name="mdp" id="mdp" type="password">
-    <input type="submit" id="connect" name="connect" value="Se connecter">
-</form>
+<div class="home-container align">
+  <div class="home-body col-md-3 animated fadeInLeft">
+    <img class="home-logo" src="/src/img/home-logo.png">
+
+    <!-- Formulaire -->
+    <form action="" method="post">
+      <div class="gfi-input home-input">
+        <input name="mail" id="mail" type="text" class="gfi-input-text" required/>
+        <span class="gfi-input-label">E-mail</span>
+      </div>
+      <div class="gfi-input home-input">
+        <input name="mdp" id="mdp" type="password" name="" class="gfi-input-text" required/>
+        <span class="gfi-input-label">Mot de passe</span>
+      </div>
+      <button class="btn gfi-btn gfi-btn-primary home-btn full-width" name="connect" id="connect">
+          <span class="gfi-btn-span firstspan">Se connecter</span>
+          <span class="gfi-btn-span secondspan">Se connecter</span>
+      </button>
+    <!-- Fin formulaire -->
+
+    <div class="gfi-body-footer">
+      <span class="home-cgu-link">CGU</span>
+      <span class="home-subscribe-link">S'inscrire</span>
+    </div>
+  </div>
+</div>
